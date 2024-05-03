ff2d7832e3f4ecb90331629ae3433acc5ccd870b
D Dharma Teja <97278903+Teja9398@users.noreply.github.com>
2024-05-03 20:50:11 +0530

    Update Palette.jsx

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,23 +1,4 @@
-// import React, { useEffect } from 'react'
 
-
-// function Palette(props) {
-
-//   return (
-//    <>
-//    <div id="needUpdate" className={`flex bg-white w-64 h-20 rounded-sm border-l-[4px] border-black`}>
-//       <span className={`flex flex-col pl-2 justify-center`}>
-//         <label className='text-green-800 text-sm font-semibold'>{props.title}</label>
-//         <label className='font-bold text-gray-700'>{props.numerator}/{props.denominator}</label>
-//         <label className="text-xs font-semibold text-gray-600">Today/Period</label>
-//       </span>
-//       <span id = "hello" className={`m-auto rounded-full p-[14px] ml-auto mr-[30px] `}><i>{props.icon}</i></span>
-//    </div>
-//    </>
-//   )
-// }
-
-// export default Palette
 import React, { useEffect, useRef } from 'react';
 
 function Palette(props) {
